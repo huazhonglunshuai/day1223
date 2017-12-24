c55323b498e1071634487b7d848a951bd97f699e
王 <@DESKTOP-M11G36C>
2017-12-24 14:15:00 +0800

    第四次提交

--- src/com/wwj/git/test/Test01.java
+++ src/com/wwj/git/test/Test01.java
@@ -1,5 +1,8 @@
 package com.wwj.git.test;
 
 public class Test01 {
-
+	
+	public void test001(){
+		
+	}
 }
--- src/com/wwj/git/test/Test02.java
+++ src/com/wwj/git/test/Test02.java
@@ -2,8 +2,5 @@ package com.wwj.git.test;
 
 public class Test02 {
 	
-	public void test001(){
-		
-	}
 	
 }
